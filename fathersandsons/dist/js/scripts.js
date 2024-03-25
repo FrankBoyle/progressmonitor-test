8b7438a8a68ed679d13854369fa5f8bbc3fda64a
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-03-25 10:22:22 -0400

    Update scripts.js

--- fathersandsons/dist/js/scripts.js
+++ fathersandsons/dist/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-
 $(document).ready(function() {
     $('#registrationForm').submit(function(e) {
         e.preventDefault(); // Prevent the default form submission
