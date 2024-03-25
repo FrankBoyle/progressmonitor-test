045b369cbb53abcdef1f4f94409e262d7fb504ae
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-03-25 12:01:06 -0400

    Update admin.php

--- fathersandsons/dist/admin.php
+++ fathersandsons/dist/admin.php
@@ -43,18 +43,13 @@
 
         <!-- Content section 1-->
         <section id="calendar">
-          <div class="container px-5">
-              <div class="row gx-5 align-items-center">
-                  <div class="col-lg-6 order-lg-2">
-                  <div id="calendar-container">
-                  <div id='calendar'></div>
-              </div>
-
-                  </div>
-                  <div class="col-lg-6 order-lg-1">
-                    <div class="p-5">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div id="calendar-container">
+                            <div id='calendar'></div>
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </section>
