1327fa225dacfa6cbdf17bc80b48d7b564ddad8e
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-03-25 13:06:34 -0400

    Update admin.php

--- fathersandsons/dist/admin.php
+++ fathersandsons/dist/admin.php
@@ -18,8 +18,6 @@
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.1.11/main.min.css" rel="stylesheet" />
-
         <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
