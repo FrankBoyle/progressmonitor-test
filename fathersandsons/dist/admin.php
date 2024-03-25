5441c6fccaa0c0848ad6a41d91f8d37a072b716b
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-03-25 12:51:39 -0400

    Update admin.php

--- fathersandsons/dist/admin.php
+++ fathersandsons/dist/admin.php
@@ -10,8 +10,9 @@
                 <!-- Bootstrap core JS-->
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
         <script src="./js/scripts.js"></script>
         <!-- Font Awesome icons (free version)-->
