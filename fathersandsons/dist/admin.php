204575103480ebceb3acdcf92c41378792585535
FrankBoyle <126579619+FrankBoyle@users.noreply.github.com>
2024-03-25 12:43:39 -0400

    Update admin.php

--- fathersandsons/dist/admin.php
+++ fathersandsons/dist/admin.php
@@ -49,11 +49,11 @@
         <section id="calendar">
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6 order-lg-2">
+
                         <div id="calendar-container" class="calendar-custom">\
                             <div id='calendar'></div>
                         </div>          
-                    </div>
+
                 </div>
             </div>
         </section>
